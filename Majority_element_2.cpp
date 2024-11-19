207089ef5af90ac34f126418c9a21330cc01160d
PrajwalSinghBhadouria <151111886+PrajwalSinghBhadouria@users.noreply.github.com>
2024-11-20 04:28:49 +0530

    Add files via upload

---
+++ Majority_element_2.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to find the majority elements in the array
+    vector<int> findMajority(vector<int>& arr) {
+        // Your code goes here.
+        int n = arr.size();
+        map<int,int> mp;
+        for(int i=0; i<n; i++){
+            mp[arr[i]]++;
+        }
+        vector<int> v;
+        for(auto i:mp){
+            if((n/3)<i.second){
+                v.push_back(i.first);
+            }
+        }
+        sort(v.begin(),v.end());
+        return v;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+
+        string s;
+        getline(cin, s);
+        stringstream ss(s);
+        vector<int> nums;
+        int num;
+        while (ss >> num) {
+            nums.push_back(num);
+        }
+        Solution ob;
+        vector<int> ans = ob.findMajority(nums);
+        if (ans.empty()) {
+            cout << "[]";
+        } else {
+            for (auto &i : ans)
+                cout << i << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
